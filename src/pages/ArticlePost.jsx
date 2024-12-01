86108b5c0fe9197710467624b39393a539cbf471
unknown <tengku.rayhan26@gmail.com>
2024-12-02 05:16:55 +0800

    fix error

--- src/pages/ArticlePost.jsx
+++ src/pages/ArticlePost.jsx
@@ -1,4 +1,5 @@
 // src/pages/ArticlePost.jsx
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
--- src/pages/services/DeliverGoods.jsx
+++ src/pages/services/DeliverGoods.jsx
@@ -1,4 +1,5 @@
 // src/pages/DeliverGoods.jsx
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
