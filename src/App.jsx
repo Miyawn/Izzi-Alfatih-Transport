3c5d528bf3a05fb31a42fe11457a05a168f4729a
Miyawn <tengku.rayhan26@gmail.com>
2025-12-09 21:18:14 +0800

    solve deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import AboutPage from './pages/AboutPage';
 import ServicesPage from './pages/ServicesPage';
 import ArticlesPage from './pages/ArticlesPage';
